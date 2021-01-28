df762d2a9f9cd8ea192b6bf19c5448dde61fa623
Ibrahim <m99hassa@uwaterloo.ca>
2021-01-28 01:37:18 -0500

    Adding the CircleCI config file to intialize CI. Also added a hello_world python file that will act as a placeholder for tests for now.

---
+++ tests/test_hello_world.py
@@ -0,0 +1,4 @@
+# This is a test file to get CircleCI up and running, this will be replaced later on by 
+# an actual test that will validate the software deployment/intialization
+
+print("hello_world")
