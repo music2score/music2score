1784c05ee0cf326acf180da03de62568550ffd37
Christopher Gus Mannes <cgmannes@uwaterloo.ca>
2021-02-08 11:11:59 -0500

    Navbar complete.

---
+++ music2score/src/components/Tabnav.js
@@ -0,0 +1,14 @@
+// Imports.
+import React from 'react'
+
+class Tabnav extends React.Component {
+    render() {
+        return (
+            <div style={{width: '30%'}}>
+
+            </div>
+        );
+    }
+}
+
+export default Tabnav;
