19b694d8d3086d096e74478ff0a9f81654620643
Ze Sheng <z7sheng@uwaterloo.ca>
2021-02-24 00:05:48 +0800

    Edited the polling script to be a temporary server

--- python/polling.py
+++ python/polling.py
@@ -1,5 +1,6 @@
-from time import sleep
+from time import sleep, time
 from collections import deque
+from pickle import dump, load
 
 from .constants import *
 from .jobs import *
@@ -9,11 +10,11 @@ from .upload import *
 
 
 # Retrieve and claim jobs
-def polling(jobList: deque) -> bool:
+def polling(trigger: bool, jobList: deque) -> bool:
     mydb = conn.connect(dbTest)
 
     var = True
-    while var:
+    while trigger and var:
         mycursor = mydb.cursor()
         fetchFlag, myresult = fetch_job(mycursor)
         mycursor.close()
@@ -39,6 +40,25 @@ def polling(jobList: deque) -> bool:
             var = True
             sleep(pollNormalDelay)
         
-    print("Polling system has terminated.")
-    print("len(jobList) =", len(jobList))
-    return False
+    if var:
+        return True
+    else:
+        print("Polling system has terminated.")
+        print("len(jobList) =", len(jobList))
+        return False
+
+
+if __name__ == '__main__':
+    trigger = True
+    jobList = deque()
+
+    polling(trigger, jobList)
+
+    if not os.path.exists("logs"):
+        os.makedirs("logs")
+
+    # Save the jobList
+    logDir = "logs\\log_jobList_%s.pkl" %round(time())
+    dump(jobList, open(logDir, "wb"), protocol=4)
+    print("'jobList' has been saved:", jobList)
+    print("Exited.")
