be4f348f89e886cf5fd4c2fd1bd78400334d2e6b
Tracyee <caiyeee@outlook.com>
2021-02-27 22:01:57 +0800

    modified convert script

--- python/convert.py
+++ python/convert.py
@@ -1,5 +1,25 @@
 # #converts midi to png and pdf
 # print("Step 2: convert midi to pdf and png.")
 
-def convert():
-    pass
+import subprocess
+from .jobs import *
+
+
+def convert(newJob: JOB) -> bool:
+    midiName = newJob.localFilePath() + ".mid"
+    lyName = newJob.localFilePath() + ".ly"
+    pdfName = newJob.localFilePath() + ".pdf"
+    pngName = newJob.localFilePath() + ".png"
+
+    cmd1 = "midi2ly " + midiName + " -o " + lyName
+    cmd2 = "lilypond -fpng " + lyName
+    cmd3 = "lilypond -fpdf " + lyName
+    cmd = cmd1 + "; " + cmd2 + "; " + cmd3
+    res = 
+    try:
+        subprocess.run(cmd, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print("stderr: ", e.stderr)
+        return False
+    
+    return True
