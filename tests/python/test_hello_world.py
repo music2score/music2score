e0139edd11c32fcf78bcd3db53339600824945e7
Ze Sheng <z7sheng@uwaterloo.ca>
2021-03-05 00:38:44 +0800

    Restored 'test_hello_world.py'

--- tests/python/test_hello_world.py
+++ tests/python/test_hello_world.py
@@ -1,9 +1,4 @@
 # This is a test file to get CircleCI up and running, this will be replaced later on by 
 # an actual test that will validate the software deployment/intialization
 
-import sys
-sys.path.append("..\\..\\")
-
-from python import *
-
 print("hello_world")
