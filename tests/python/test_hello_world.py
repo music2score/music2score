f423b90d71a353d7db8c5b5fd326900db5d91a07
Sai Anurag <saianurag.n@gmail.com>
2021-03-13 22:09:58 +0530

    added mydb.py to test database

--- tests/python/test_hello_world.py
+++ tests/python/test_hello_world.py
@@ -1,4 +1,4 @@
 # This is a test file to get CircleCI up and running, this will be replaced later on by 
 # an actual test that will validate the software deployment/intialization
-
+from python.constants import *
 print("hello_world")
--- tests/python/unit/mydb.py
+++ tests/python/unit/mydb.py
@@ -33,3 +33,6 @@ class Cursor():
         return (11,'sample.midi','user1',0,0,'1000-01-01 00:00:00')
     def close(self):
         pass
+class mysql():
+    def __init__(self):
+        pass
