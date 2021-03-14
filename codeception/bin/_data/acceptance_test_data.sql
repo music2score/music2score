1ba5223a11f45284c7e9341771c7c50dd329dd7a
Ibrahim <m99hassa@uwaterloo.ca>
2021-03-13 20:44:46 -0500

    Saving

--- codeception/bin/_data/acceptance_test_data.sql
+++ codeception/bin/_data/acceptance_test_data.sql
@@ -2,3 +2,5 @@ USE music2score_test;
 
 INSERT IGNORE INTO user (fname, lname, email, pass)
 VALUES ('test', 'user', 'testuser@test.com', 1234);
+
+-- docker exec -i docker_mysql_server_1 mysql -u root -p12345 < ../codeception/bin/_data/acceptance_test_data.sql
