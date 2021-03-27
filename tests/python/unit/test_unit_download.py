f21ad5cff1ef96ee12245768ee4bac0384d2d611
Sai Anurag <saianurag.n@gmail.com>
2021-03-27 18:47:07 +0530

    modified unit tests

--- tests/python/unit/test_unit_download.py
+++ tests/python/unit/test_unit_download.py
@@ -5,6 +5,7 @@ import datetime
 import unittest
 from unittest import TestCase
 import requests
+import shutil
 
 # Standard library imports...
 from unittest.mock import Mock, patch
@@ -13,11 +14,41 @@ from unittest.mock import Mock, patch
 class TestDownload(TestCase):
   
     def test_status_200_error_in_response(self):
-        pass
+        mynewjob = JOB()
+        myrestuple=(1, 'sample.mid', 1, 0, 0, datetime.datetime(2021, 3, 23, 0, 0))
+        mynewjob.set_job(myrestuple)
+        with patch('python.download.requests.post') as mock_post:
+            # Configure the mock with a error in response.
+            stri="\x00"+"\x00"
+            data=bytes(stri,'UTF-8')
+            mock_post.return_value.status_code=200
+            #mock_post.return_value=data
+            mock_post.return_value.content=data
+            mock_post.return_value.OK =True
+            
+            ret=download_src(mynewjob)
+        self.assertFalse(ret)
 
     def test_status_200_good_response(self):
-        pass
-    
+        mynewjob = JOB()
+        myrestuple=(1, 'sample.mid', 1, 0, 0, datetime.datetime(2021, 3, 23, 0, 0))
+        mynewjob.set_job(myrestuple)
+        with patch('python.download.requests.post') as mock_post:
+            # Configure the mock with a error in response.
+            stri="MThd"+"\x00"+"\x00"
+            data=bytes(stri,'UTF-8')
+            mock_post.return_value.status_code=200
+            mock_post.return_value.content=data
+            mock_post.return_value.OK =True
+            
+            ret=download_src(mynewjob)
+            self.assertTrue(ret)
+
+            retval=os.path.exists('/project/tests/python/1' +"/" + 'sample.mid')
+            self.assertTrue(retval)
+            if os.path.exists('/project/tests/python/1'):
+                shutil.rmtree('/project/tests/python/1')
+        
     def test_status_not_200(self):
         mynewjob = JOB()
         myrestuple=(1, 'sample.mid', 1, 0, 0, datetime.datetime(2021, 3, 23, 0, 0))
