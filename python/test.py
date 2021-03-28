10613c1aa67a2886c410bc74e8fb01d59cdacd1a
Ibrahim <m99hassa@uwaterloo.ca>
2021-03-28 02:44:13 -0400

    testing db connection.

--- python/test.py
+++ python/test.py
@@ -4,8 +4,21 @@ mydb = mysql.connector.connect(
             host="mysql-server",
             user="root",
             password="12345",
-            database="music2score"
+            database="music2score_test"
         )
 
 mydb.is_connected()
 print(mydb.is_connected())
+
+# sql = "INSERT INTO jobs (jobid, filename, userid, processing, completed, date) \
+#                VALUES (%s, %s, %s, %s, %s, %s)"
+# vals = (1, 'sample.mid', 1, 0, 0, '2021-03-23')
+# mydb.mycursor.execute(sql, vals)
+# mydb.commit()
+
+sql = "show databases"
+mydb.mycursor.execute(sql, vals)
+mydb.commit()
+
+ans = mydb.mycursor.fetchone()
+print(ans)
