9a21e08a1251d541d74ee18fc45be962f3962157
Ibrahim <m99hassa@uwaterloo.ca>
2021-04-11 15:36:57 -0400

    readded changes

--- tests/codeception/acceptance/NavBarCest.php
+++ tests/codeception/acceptance/NavBarCest.php
@@ -18,6 +18,7 @@ class NavigationBarCest
         $I->fillField('email', 'testuser@test.com');        
         $I->fillField('password', '1234');
         $I->click('#loginbtn');
+        $I->wait(1);
         $I->seeCurrentUrlEquals('/');
     }
 
@@ -77,26 +78,30 @@ class NavigationBarCest
         $this->_login($I);
         $I->see('Account');
         $I->click('Account');
+        $I->wait(2);
         $I->see('Upload New');
         $I->see('My Sheets');
         $I->see('Logout');
 
         $I->click('Upload New');
+        $I->wait(2);
         $I->seeCurrentUrlEquals('/upload.php');
 
-        $I->wait(1);
+        $I->wait(2);
         $I->click('Account');
-        $I->wait(0.5);
+        $I->wait(2);
         $I->see('My Sheets');
-        $I->wait(0.5);
+        $I->wait(2);
         $I->click('My Sheets');
+        $I->wait(2);
         $I->seeCurrentUrlEquals('/download.php');
 
-        $I->wait(1);
+        $I->wait(2);
         $I->click('Account');
-        $I->wait(1);
+        $I->wait(2);
         $I->see('Logout');
         $I->click('Logout');
+        $I->wait(2);
         $I->seeCurrentUrlEquals('/');
         $I->see('Sign In/Up');
         $I->dontSee('Account');
